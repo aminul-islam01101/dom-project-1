5e731e1e52b2c53a993550f0c7f35fdd8543129b
aminulislam01101 <aminul.islam01101@gmail.com>
2022-05-27 01:36:31 +0600

    animated copy toast message added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // /* eslint-disable indent */
+let div = null;
 
 function generateColor() {
     const red = Math.floor(Math.random() * 256).toString(16);
@@ -7,6 +8,20 @@ function generateColor() {
     return `#${red}${green}${blue}`;
 }
 
+function generateToastMessage(msg) {
+    div = document.createElement('div');
+    div.innerText = msg;
+    document.body.appendChild(div);
+    div.className = 'toast-message toast-message-slide-in';
+    div.addEventListener('click', () => {
+        div.classList.remove('toast-message-slide-in');
+        div.classList.add('toast-message-slide-out');
+        div.addEventListener('animationend', () => {
+            div.remove();
+            div = null;
+        });
+    });
+}
 function main() {
     const button = document.querySelector('button[type="button"]');
     const output = document.querySelector('#hex-code');
@@ -19,6 +34,10 @@ function main() {
     });
     copyButton.addEventListener('click', () => {
         navigator.clipboard.writeText(output.value);
+        if (div !== null) {
+            div.remove();
+        }
+        generateToastMessage(`${output.value} copied`);
     });
 }
 
