9e8fa4e0b6674c0bf51685b4d4bd5053ec8640c3
aminulislam01101 <aminul.islam01101@gmail.com>
2022-05-30 00:13:01 +0600

    rgb color added with hex and debugging

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
 /* eslint-disable no-alert */
 // /* eslint-disable indent */
+
 let div = null;
 
-function generateColor() {
-    const red = Math.floor(Math.random() * 256)
-        .toString(16)
-        .toUpperCase();
-    const green = Math.floor(Math.random() * 256)
-        .toString(16)
-        .toUpperCase();
-    const blue = Math.floor(Math.random() * 256)
-        .toString(16)
-        .toUpperCase();
-    return `#${red}${green}${blue}`;
+// random decimal color function-1
+
+function generateDecimalColor() {
+    const red = Math.floor(Math.random() * 256);
+
+    const green = Math.floor(Math.random() * 256);
+
+    const blue = Math.floor(Math.random() * 256);
+
+    return { red, green, blue };
 }
+
+// hexadecimal color function-2
+
+function generateHexColor({ red, green, blue }) {
+    const getTwoCode = (value) => {
+        const hex = value.toString(16);
+
+        return hex.length === 1 ? `0${hex}` : hex;
+    };
+    return `#${getTwoCode(red)}${getTwoCode(green)}${getTwoCode(blue)}`.toUpperCase();
+}
+
+// hexadecimal color function-3
+
+function generateRGBColor({ red, green, blue }) {
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+// toast -message-slide-in function-4
 
 function generateToastMessage(msg) {
     div = document.createElement('div');
     div.innerText = msg;
     document.body.appendChild(div);
     div.className = 'toast-message toast-message-slide-in';
+
+    // toast -message-slide- addEventListener
+
     div.addEventListener('click', () => {
         div.classList.remove('toast-message-slide-in');
         div.classList.add('toast-message-slide-out');
+
+        // copy Button animation addEventListener
+
         div.addEventListener('animationend', () => {
             div.remove();
             div = null;
         });
     });
 }
+
+// hex color validation function-5
 /**
  * @param {string} color
  */
@@ -39,39 +66,78 @@ function isValidHex(color) {
 
     return /^([A-Fa-f0-9]{8}|[A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/i.test(color);
 }
+// Main function
 
 function main() {
-    const button = document.querySelector('button[type="button"]');
-    const input = document.querySelector('#hex__input');
-    const bodyColor = document.querySelector('body');
-    const copyButton = document.querySelector('#copy-button');
-    button.addEventListener('click', () => {
-        const bgColor = generateColor();
+    // variables declaration
 
-        bodyColor.style.backgroundColor = bgColor;
-        input.value = bgColor.substring(1);
+    const button = document.querySelector('button[type="button"]');
+
+    const input = document.querySelector('#hex__input');
+
+    const rgbInput = document.querySelector('#rgb__input');
+
+    const bodyColor = document.querySelector('body');
+
+    const copyButton = document.querySelector('#copy-button');
+
+    const rgbCopyButton = document.querySelector('#rgb-copy-button');
+
+    // color change button event addEventListener-1
+
+    button.addEventListener('click', () => {
+        const color = generateDecimalColor();
+
+        const hex = generateHexColor(color);
+
+        const rgb = generateRGBColor(color);
+
+        bodyColor.style.backgroundColor = hex;
+        input.value = hex.substring(1);
+        rgbInput.value = rgb;
     });
+
+    // hex copyButton addEventListener-2
+
     copyButton.addEventListener('click', () => {
         navigator.clipboard.writeText(`#${input.value}`);
+
         if (div !== null) {
             div.remove();
         }
+
         if (isValidHex(input.value)) {
             generateToastMessage(`#${input.value} copied`);
         } else {
             alert('Invalid color code');
         }
     });
+
+    // rgb color copyButton addEventListener-3
+    rgbCopyButton.addEventListener('click', () => {
+        navigator.clipboard.writeText(rgbInput.value);
+        if (div !== null) {
+            div.remove();
+        }
+        generateToastMessage(`${rgbInput.value} copied`);
+    });
+
+    // dynamic hex color  input addEventListener-4
+
     input.addEventListener('keyup', (event) => {
         const color = event.target.value;
+
         if (color) {
             input.value = color.toUpperCase();
         }
+
         if (color && isValidHex) {
             bodyColor.style.backgroundColor = `#${color}`;
         }
     });
 }
+// Main function execution
+
 window.onload = () => {
     main();
 };
